e128e1edc415acccc2f02a731de7d5b10d413728
Mark Frey <mark@bearded.com>
2013-12-03 11:33:25 -0500

    Make mixin has_many's consistent, use scope block instead of deprecated params.
    [#18 state:resolved]

--- lib/trim/models/has_images.rb
+++ lib/trim/models/has_images.rb
@@ -2,7 +2,7 @@ module Trim
   module HasImages
     
     def has_images
-      has_many :images, :as => :imageable, :dependent => :destroy, :order => :sort, :class_name => 'Trim::Image'
+      has_many :images, -> { order('trim_images.sort ASC') }, :as => :imageable, :class_name => 'Trim::Image', :dependent => :destroy
       accepts_nested_attributes_for :images, :allow_destroy => true
     end
 
--- lib/trim/models/has_related_items.rb
+++ lib/trim/models/has_related_items.rb
@@ -2,7 +2,7 @@ module Trim
   module HasRelatedItems
     
     def has_related_items
-      has_many :related_items, :as => :related_from, :inverse_of => :related_from, :dependent => :destroy, :order => :sort, :class_name => 'Trim::RelatedItem'
+      has_many :related_items, -> { order('trim_related_items.sort ASC') }, :as => :related_from, :class_name => 'Trim::RelatedItem', :inverse_of => :related_from, :dependent => :destroy
       has_many :relating_items, :class_name => 'Trim::RelatedItem', :as => :related_to, :inverse_of => :related_to
       
       accepts_nested_attributes_for :related_items, :allow_destroy => true
