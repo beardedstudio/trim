25e34256f980fcfdfb280560b86f8d7d58362bf4
Brett Bender <brett@bearded.com>
2013-12-06 15:01:25 -0500

    Fix has-many declarations that would be used in rails4 for rails3 style

--- lib/trim/models/has_downloads.rb
+++ lib/trim/models/has_downloads.rb
@@ -1,8 +1,8 @@
 module Trim
   module HasDownloads
-    
+
     def has_downloads
-      has_many :downloads, -> { order('trim_downloads.sort ASC') }, :as => :downloadable, :class_name => 'Trim::Download', :dependent => :destroy
+      has_many :downloads, :as => :downloadable, :class_name => 'Trim::Download', :dependent => :destroy, :order => 'trim_downloads.sort ASC'
       accepts_nested_attributes_for :downloads, :allow_destroy => true
     end
 
--- lib/trim/models/has_related_items.rb
+++ lib/trim/models/has_related_items.rb
@@ -1,10 +1,10 @@
 module Trim
   module HasRelatedItems
-    
+
     def has_related_items
-      has_many :related_items, -> { order('trim_related_items.sort ASC') }, :as => :related_from, :class_name => 'Trim::RelatedItem', :inverse_of => :related_from, :dependent => :destroy
+      has_many :related_items, :as => :related_from, :class_name => 'Trim::RelatedItem', :inverse_of => :related_from, :dependent => :destroy, :order => 'trim_related_items.sort ASC'
       has_many :relating_items, :class_name => 'Trim::RelatedItem', :as => :related_to, :inverse_of => :related_to
-      
+
       accepts_nested_attributes_for :related_items, :allow_destroy => true
     end
 
