bc7adb321c20b396f2fb25c27ba70684665c723d
Brett Bender <brett@bearded.com>
2013-12-19 16:30:03 -0500

    Add explicit compilation instruction for asset pipeline.

--- lib/trim/engine.rb
+++ lib/trim/engine.rb
@@ -9,6 +9,17 @@ module Trim
       end
     end
 
+    # Enabling assets precompiling under rails 3.1
+    if Rails.version >= '3.1'
+      initializer :assets do |config|
+        Rails.application.config.assets.precompile += %w( trim.js
+                                                          trim.css
+                                                          errors.css
+                                                          no-mq.css
+                                                          admin-bar.scss)
+      end
+    end
+
     config.generators do |g|
       g.test_framework      :rspec,        :fixture => false
       g.assets false
