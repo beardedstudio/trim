9567d85293a2d1e46c7c52a259bb75f5fceb0bcc
Mark Frey <mark@bearded.com>
2014-02-26 13:30:54 -0500

    Remove unused rails admin actions + dependencies

--- lib/trim.rb
+++ lib/trim.rb
@@ -23,11 +23,8 @@ module Trim
 
   # Rails Admin custom actions
   require 'trim/rails_admin/rails_admin_multi_enum.rb'
-  require 'trim/rails_admin/rails_admin_nested_sort.rb'
-  require 'trim/rails_admin/rails_admin_reorder.rb'
   require 'trim/rails_admin/rails_admin_settings.rb'
   require 'trim/rails_admin/rails_admin_show_in_app.rb'
-  require 'trim/rails_admin/rails_admin_single_hierarchy.rb'
 
   # Mixins
   require 'trim/models/has_excerpt.rb'
